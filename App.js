f5cbae2ec55ea5a5ccad579e4f7f13f2e25b06de
Manmoksh <mamoksh04@gmail.com>
2024-08-06 20:22:39 +0530

    Starting learning react

---
+++ App.js
@@ -0,0 +1,7 @@
+const heading = React.createElement(
+  "h1",
+  { className: "root" },
+  "helllo world from react!"
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
