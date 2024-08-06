0081615e5d257b0d403a4097cf4c91a9c49370e5
Manmoksh <mamoksh04@gmail.com>
2024-08-06 21:32:52 +0530

    Use parcel to ignite the app

--- App.js
+++ App.js
@@ -1,7 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const heading = React.createElement(
   "h1",
   { className: "root" },
-  "helllo world from react!"
+  "helllo world from react! hi "
 );
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
