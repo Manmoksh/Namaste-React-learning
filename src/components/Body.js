8292b3901bf3a7f8a2617675334b8216380e3d6f
Manmoksh <mamoksh04@gmail.com>
2024-08-11 12:07:39 +0530

    Add routes to [roject

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,7 +4,8 @@ import Shimmer from "./Shimmer";
 
 export default function Body() {
   const [listOfRestaurants, setListOfRestaurants] = useState([]);
-
+  const [filteredRestaurant, setFilteredRestaurant] = useState([]);
+  const [searchText, setSearchText] = useState("");
   useEffect(() => {
     fetchData();
   }, []);
@@ -17,15 +18,38 @@ export default function Body() {
     setListOfRestaurants(
       json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
     );
+    setFilteredRestaurant(
+      json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
   };
-  if (listOfRestaurants.length === 0) {
-    return <Shimmer />;
-  }
+  //conditional rendering
 
-  return (
+  return listOfRestaurants.length === 0 ? (
+    <Shimmer />
+  ) : (
     <main className="body">
       <div className="search-bar">
-        Search
+        <input
+          type="text"
+          className="search-box"
+          value={searchText}
+          placeholder="Enter restaurant name"
+          onChange={(e) => {
+            setSearchText(e.target.value);
+          }}
+        />
+        <button
+          className="search-btn"
+          onClick={() => {
+            console.log(searchText);
+            const filterList = listOfRestaurants.filter((res) =>
+              res.info.name.toLowerCase().includes(searchText.toLowerCase())
+            );
+            setFilteredRestaurant(filterList);
+          }}
+        >
+          Search
+        </button>
         <div className="filter">
           <button
             onClick={() => {
@@ -41,7 +65,7 @@ export default function Body() {
         </div>
       </div>
       <div className="cards-container">
-        {listOfRestaurants.map((res) => (
+        {filteredRestaurant.map((res) => (
           <RestaurantCard resData={res} key={res.info.id} />
         ))}
       </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,11 @@
+import { useEffect, useState } from "react";
 import logo from "../public/logo.jpg";
 function Header() {
+  const [btnName, setBtnName] = useState("Login");
+  // if no dependency array =>useEffect called at every render
+  //if dependency error is empty =>useEffect called once on initial render
+  // if something in depenedency array => useEffect is called when btnRect change
+  // useEffect(() => {}, [btnRect]);
   return (
     <header className="header">
       <div className="logo-container">
@@ -8,10 +14,10 @@ function Header() {
       <nav className="nav-items">
         <ul>
           <li>
-            <a href="#">Home</a>
+            <a href="/">Home</a>
           </li>
           <li>
-            <a href="#">About Us </a>
+            <a href="/about">About</a>
           </li>
           <li>
             <a href="#">Contact Us</a>
@@ -19,6 +25,14 @@ function Header() {
           <li>
             <a href="#">Cart</a>
           </li>
+          <button
+            onClick={() => {
+              btnName === "Login" ? setBtnName("LogOut") : setBtnName("Login");
+            }}
+            className="login"
+          >
+            {btnName}
+          </button>
         </ul>
       </nav>
     </header>
